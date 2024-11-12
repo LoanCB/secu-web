81a1cca381728ee57f608509347b3b7a0776bbbe
loan <loan.courchinoux-billonnet@koesio.com>
2024-11-12 14:01:04 +0100

    fix: :coffin: remove dead code

--- vulnerable/back/src/common/helpers/errors.config.ts
+++ vulnerable/back/src/common/helpers/errors.config.ts
@@ -1,14 +1,12 @@
 import commonErrorCodes, { AuthErrorCodes } from 'src/auth/helpers/auth-error-codes.config';
 import userErrorCodes, { UserErrorCodes } from 'src/users/helpers/user-error-codes.config';
 import authErrorCodes, { CommonErrorCodes } from './common-error-codes.config';
-import coreErrorCodes, { CoreErrorCodes } from 'src/core/helpers/core-error-code.config';
 
-export type ErrorCode = UserErrorCodes | AuthErrorCodes | CommonErrorCodes | CoreErrorCodes;
+export type ErrorCode = UserErrorCodes | AuthErrorCodes | CommonErrorCodes;
 export type DynamicMessage = (...args: any) => string;
 
 export default () => ({
   ...userErrorCodes(),
   ...authErrorCodes(),
   ...commonErrorCodes(),
-  ...coreErrorCodes(),
 });
